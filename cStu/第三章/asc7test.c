142062d811524a2b947a01ca8e5452b579de720b
zhangzhibang0309 <295854713@qq.com>
2021-10-05 22:28:52 +0800

    更新

---
+++ cStu/第三章/asc7test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = '\a';
+
+    printf("%c", ch);
+    getchar();
+    
+    return 0;
+}
---
+++ cStu/第三章/printfCh.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//    char ch = 'a';
+//
+//    printf("%c's ascii is %d",ch,ch);
+//
+//    return 0;
+//}
